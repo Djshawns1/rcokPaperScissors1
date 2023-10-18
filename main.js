e33d09adab4a376bf63258dec69665630e7b1628
Djshawn1 <juanshawn02@yahoo.com>
2023-10-18 14:10:13 +0900

    Title: Implement Image Selection and Setup Event Listeners
    Description:
    - Adjusted HTML structure to improve semantics and accessibility:
      - Added aria-label attributes to button elements.
      - Updated image alt text for better description.
    - Modified CSS to improve layout and responsiveness:
      - Changed flex-direction to row for a horizontal layout.
      - Updated width and height units to vh for responsive design.
    - Set up JavaScript event listeners for image selection:
      - Created event listeners on image elements to capture player choice on click.
      - Updated humanPlayer function to determine player choice based on clicked image.
    - Cleaned up file paths and corrected typographical errors.
    - Prepared codebase for further JavaScript logic integration for game functionality.

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 const CHOICES = ["rock", "paper", "scissors"];
 const ROUNDS = 5;
-
+//computer choose rock, paper or scissors based on the random choosing of 1, 2, or 3
 function getComputerChoice() {
     return CHOICES[Math.floor(Math.random() * 3)];
 }
+//human player click an img of a child holding rock, paper or scissors
+let humanChoiceRock = document.querySelector(".rockImg");
+humanChoiceRock.addEventListener("click", humanPlayer)
+let humanChoicePaper = document.querySelector(".paperImg");
+humanChoicePaper.addEventListener("click", humanPlayer)
+let humanChoiceScissors = document.querySelector(".scissorsImg");
+humanChoiceScissors.addEventListener("click", humanPlayer)
 
+// event listener
 function humanPlayer() {
-    let hand = prompt("Enter rock, paper, or scissors");
-    return hand ? hand.toLowerCase() : null;
+    if (humanChoicePaper){
+        return humanChoicePaper;
+    }
+    else if (humanChoiceRock){
+        return humanChoiceRock;
+    }
+    else return humanChoiceScissors
 }
-
+//check if user inputs a valid text input
+//may need deletion later
 function isValidChoice(choice) {
     return CHOICES.includes(choice);
 }
-
+//player vs computer winner is decided and text appears in the console
 function determineWinner(computerChoice, playerChoice) {
     if (computerChoice === playerChoice) return "It's a tie";
 
@@ -63,3 +77,6 @@ function determineWinner(computerChoice, playerChoice) {
 
 // // Example call
 // console.log(playRound());
+
+
+//next add text or prompt for who won and how many rouncs
